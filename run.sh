1e076eb9700a1955c31e4300237408fbc2995ffa
lucifer1708 <20bce091@nith.ac.in>
2022-12-29 13:10:59 +0530

    resize option added to 1920

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-ls images/ | grep -v .webp | grep .$1 | cut -d '.' -f1 | xargs -I {} convert images/{}.$1  images/{}.webp;
+ls images/ | grep -v .webp | grep .$5 | cut -d '.' -f1 | xargs -I {} convert images/{}.$1  -resize 1920 images/{}.webp;
 
